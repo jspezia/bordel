f5987ad52375c33f22f4c1861e747229ab99d5c6
Jean SPEZIA <jspezia@e3r9p9.42.fr>
2015-01-28 18:10:27 +0100

    p

---
+++ c/Fract_ol/includes/fract.h
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fract.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jspezia <jspezia@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/28 15:01:51 by jspezia           #+#    #+#             */
+/*   Updated: 2015/01/28 17:28:31 by jspezia          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACT_H
+# define FRACT_H
+
+
+# define W_HEIGHT	800
+# define W_WIDTH	800
+
+# define KEY_UP		65362
+# define KEY_DOWN	65364
+# define KEY_LEFT	65361
+# define KEY_RIGHT	65363
+# define KEY_ESC	65307
+# define KEY_SHIFT	65505
+# define KEY_MORE	65451
+# define KEY_LESS	65453
+# define KEY_SPACE	32
+
+#include "libft.h"
+
+/*
+typedef enum		e_bool
+{
+	FALSE,
+	TRUE
+}					t_bool;*/
+
+typedef struct		s_img
+{
+	void			*id;
+	int				width;
+	int				height;
+	char			*addr;
+	int				bpp;
+	int				lsize;
+	int				endian;
+}					t_img;
+
+typedef struct		s_keys
+{
+	t_bool			up;
+	t_bool			down;
+	t_bool			right;
+	t_bool			left;
+}					t_keys;
+
+typedef struct		s_pt
+{
+	double			x;
+	double			y;
+}					t_pt;
+
+
+typedef struct		s_env
+{
+	void			*mlx;
+	void			*win;
+	t_img			*img;
+	t_keys			*keys;
+	t_pt			*c;
+}					t_env;
+
+#endif
--- c/Fract_ol/src/main.c
+++ c/Fract_ol/src/main.c
@@ -5,15 +5,161 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jspezia <jspezia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/01/26 14:59:19 by jspezia           #+#    #+#             */
-/*   Updated: 2015/01/26 14:59:55 by jspezia          ###   ########.fr       */
+/*   Created: 2015/01/27 15:18:38 by jspezia           #+#    #+#             */
+/*   Updated: 2015/01/28 18:09:57 by jspezia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <mlx.h>
+#include </usr/X11R6/include/X11/X.h>
+#include "fract.h"
 #include "libft.h"
+#include <unistd.h>
 
-int		main()
+
+t_env		*init_env(void)
 {
-	ft_putstr("Hello World!\n");
+	t_env	*e;
+
+	if (!(e = malloc(sizeof(t_env))))
+		exit(-1);
+	if (!(e->mlx = mlx_init()))
+		exit(-1);
+	if (!(e->win = mlx_new_window(e->mlx, W_WIDTH, W_HEIGHT, "Fractal")))
+		exit(-1);
+	if (!(e->c = malloc(sizeof(t_pt))))
+		exit(-1);
+	return (e);
+}
+
+t_img		*create_new_image(t_env *e, int width, int height)
+{
+	t_img	*img;
+
+	if (!(img = malloc(sizeof(t_img))))
+		exit(-1);
+	if (!(img->id = mlx_new_image(e->mlx, width, height)))
+		exit(-1);
+	img->width = width;
+	img->height = height;
+	img->addr = mlx_get_data_addr(img->id, &img->bpp,
+			&img->lsize, &img->endian);
+	return (img);
+}
+
+int			key_release(int keycode, t_env *e)
+{
+	(void)e;
+	if (keycode == KEY_ESC)
+		exit(0);
+	if (keycode == KEY_UP)
+		e->c->x += 0.005;
+	if (keycode == KEY_DOWN)
+		e->c->x -= 0.005;
+	if (keycode == KEY_RIGHT)
+		e->c->y += 0.005;
+	if (keycode == KEY_DOWN)
+		e->c->y -= 0.005;
+	if (keycode == KEY_SPACE)
+	{
+		printf("c = %f, %fi\n", e->c->x, e->c->y);
+	}
+	return (0);
+}
+
+
+void			my_pixel_put_to_image(t_img *img, int x, int y, int color)
+{
+	int				i;
+	int				mask;
+	char			tmp_clr;
+	int				offset;
+
+	i = 0;
+	if (x >= 0 && x < img->width && y >= 0 && y < img->height)
+	{
+		while (i < (img->bpp / 8))
+		{
+			if (img->endian == 1)
+				offset = img->bpp - 8 * (i + 1);
+			else
+				offset = 8 * i;
+			mask = 0xFF << offset;
+			tmp_clr = (color & mask) >> offset;
+			img->addr[y * (img->lsize) + x * (img->bpp / 8) + i] = tmp_clr;
+			i++;
+		}
+	}
+}
+
+
+void		clear_img(t_img *img)
+{
+	int		i;
+	int		j;
+
+	j = 0;
+	while (j < img->height)
+	{
+		i = 0;
+		while (i < img->width)
+		{
+			my_pixel_put_to_image(img, i, j, 0x000000);
+			i++;
+		}
+		j++;
+	}
+}
+
+int			fractal_julia(double x, double y, t_pt *c)
+{
+	return (0);
+}
+
+void		display_view(t_env *e, int (*ft)(double, double, t_pt *))
+{
+	int		x;
+	int		y;
+	int		color;
+	t_pt	pixel;
+
+	x = 0;
+	while (x != W_WIDTH)
+	{
+		y = 0;
+		while (y != W_HEIGHT)
+		{
+			pixel.x = (double)x - W_WIDTH / 2;
+			pixel.y = W_HEIGHT / 2 - (double)y;
+			color = ft(pixel.x, pixel.y, e->c);
+			y++;
+		}
+		x++;
+	}
+}
+
+int			loop_hook(t_env *e)
+{
+	clear_img(e->img);
+	display_view(e, &fractal_julia);
+	mlx_put_image_to_window(e->mlx, e->win, e->img->id, 0, 0);
+	return (0);
+}
+
+int			main()
+{
+	t_env		*e;
+
+	e = init_env();
+	e->c->x = 0;
+	e->c->y = 0;
+	e->img = create_new_image(e, W_WIDTH, W_HEIGHT);
+	mlx_hook(e->win, KeyRelease, KeyReleaseMask, &key_release, e);
+	mlx_loop_hook(e->mlx, &loop_hook, e);
+//	mlx_pixel_put(e->mlx,e->win,250,10,0xFFFFFF);
+	mlx_loop(e->mlx);
 	return (0);
 }
